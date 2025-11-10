1ca3c12ac8f6adf7cbd260b1047e7329756aeabf
ask <2137576215@qq.com>
2025-11-10 10:07:05 +0800

    保存

--- 北燃报表/MZ702人员年龄、技能等级、学历报表.sql
+++ 北燃报表/MZ702人员年龄、技能等级、学历报表.sql
@@ -1,8 +1,8 @@
 -- 展开组织名称，但数值按公司类型与岗位序列汇总
 select 
-  orgs.orgname,
-  agg.gslx,
+  agg.orgname,
   agg.gwxl,
+  agg.gwxl as gslx,
   agg.zs,
   agg.hz,
   agg.bshz,
@@ -26,14 +26,15 @@ select
   agg.cj,
   agg.zj,
   agg.fgj,
-  agg.zgj
+  agg.zgj,
+  cast('' as varchar(20)) as rq,
+  cast('' as varchar(200)) as rylb,
+  cast('' as varchar(200)) as zzlx,
+  cast('' as varchar(200)) as dwszd,
+  cast('' as varchar(200)) as zzmc
 from (
   select 
-    case when  bd_defdoc_zzlx.name in ('总部','分公司','专业机构','事业部') then '母公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京内' then '京内子公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京外' then '京外子公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='境外' then '境外子公司'
-    end as gslx,
+    T2.name as orgname,
     case when T1.pk_postseries in ( '10011T100000000098AQ' , '10011T100000000098AT' , '10011T100000000098AU' , '10011T100000000098AV' , '10011T100000000098AW' , '10011T100000000098AX' , '10011T100000000098AY' , '10011T100000000098AZ' ) then '经营管理'
          when T1.pk_postseries in ( '10011T100000000098AR' , '10011T100000000098B0' , '10011T100000000098BB' , '10011T100000000098BP' , '10011T100000000098BQ' , '10011T100000000098BT' , '10011T100000000098BU' , '10011T100000000098BV' , '10011T100000000098BW' , '10011T100000000098BX' , '10011T100000000098BY' , '10011T100000000098BZ' , '10011T100000000098C0' , '10011T100000000098C1' , '10011T100000000098C2' , '10011T100000000098C3' , '10011T1000000000A73M' , '10011T100000000098B1' , '10011T100000000098C4' , '10011T100000000098C5' , '10011T100000000098C6' , '10011T100000000098C7' , '10011T100000000098CH' , '10011T100000000098CI' , '10011T100000000098CJ' , '10011T100000000098CK' , '10011T100000000098CL' , '10011T100000000098CM' , '10011T100000000098CN' , '10011T100000000098CO' , '10011T100000000098CP' , '10011T100000000098CQ' , '10011T100000000098CR' , '10011T100000000098CS' , '10011T100000000098CT' , '10011T100000000098CU' , '10011T100000000098DP' , '10011T100000000098DQ' , '10011T100000000098DR' , '10011T100000000098DS' , '10011T100000000098DT' , '10011T100000000098DU' , '10011T100000000098B7' , '10011T100000000098DV' , '10011T100000000098DW' , '10011T100000000098DX' , '10011T100000000098DY' , '10011T100000000098DZ' , '10011T100000000098E0' , '10011T100000000098E1' , '10011T100000000098E2' , '10011T100000000098E3' , '10011T100000000098BA' ) then '专业技术' end as gwxl,
     count(distinct bd_psndoc.pk_psndoc) as zs,
@@ -81,16 +82,20 @@ from (
   where 1 = 1 
     and hi_psnorg.psntype = 0
     and T1.ismainjob='Y'
-    -- 时间段筛选：任职时间与查询时间段有交集
-    and T1.begindate<=parameter('param2') and nvl(T1.enddate, '2099-12-31') >= parameter('param1')
+  -- 单日筛选：任职时间包含所选日期
+  and T1.begindate<=parameter('rq') and nvl(T1.enddate, '2099-12-31') >= parameter('rq')
     and bd_psncl.name in (parameter('rylb'))
     and bd_defdoc_zzlx.name in (parameter('zzlx'))
+    and bd_defdoc_dwszd.name in (parameter('dwszd'))
+    and bd_defdoc_zzlx.name is not null
+    and bd_defdoc_dwszd.name is not null
     and T2.name in (parameter('zzmc'))
-    -- 岗位序列筛选
-    and (
-      T1.pk_postseries in ( '10011T100000000098AQ' , '10011T100000000098AT' , '10011T100000000098AU' , '10011T100000000098AV' , '10011T100000000098AW' , '10011T100000000098AX' , '10011T100000000098AY' , '10011T100000000098AZ' )  or
-      T1.pk_postseries in ( '10011T100000000098AR' , '10011T100000000098B0' , '10011T100000000098BB' , '10011T100000000098BP' , '10011T100000000098BQ' , '10011T100000000098BT' , '10011T100000000098BU' , '10011T100000000098BV' , '10011T100000000098BW' , '10011T100000000098BX' , '10011T100000000098BY' , '10011T100000000098BZ' , '10011T100000000098C0' , '10011T100000000098C1' , '10011T100000000098C2' , '10011T100000000098C3' , '10011T1000000000A73M' , '10011T100000000098B1' , '10011T100000000098C4' , '10011T100000000098C5' , '10011T100000000098C6' , '10011T100000000098C7' , '10011T100000000098CH' , '10011T100000000098CI' , '10011T100000000098CJ' , '10011T100000000098CK' , '10011T100000000098CL' , '10011T100000000098CM' , '10011T100000000098CN' , '10011T100000000098CO' , '10011T100000000098CP' , '10011T100000000098CQ' , '10011T100000000098CR' , '10011T100000000098CS' , '10011T100000000098CT' , '10011T100000000098CU' , '10011T100000000098DP' , '10011T100000000098DQ' , '10011T100000000098DR' , '10011T100000000098DS' , '10011T100000000098DT' , '10011T100000000098DU' , '10011T100000000098B7' , '10011T100000000098DV' , '10011T100000000098DW' , '10011T100000000098DX' , '10011T100000000098DY' , '10011T100000000098DZ' , '10011T100000000098E0' , '10011T100000000098E1' , '10011T100000000098E2' , '10011T100000000098E3' , '10011T100000000098BA' )
-    )
+  -- 岗位序列筛选迁移至外层：按列名 agg.gwxl 过滤
+  -- 仅保留两大岗位序列编码，排除未归类/空
+  and (
+      T1.pk_postseries in ( '10011T100000000098AQ' , '10011T100000000098AT' , '10011T100000000098AU' , '10011T100000000098AV' , '10011T100000000098AW' , '10011T100000000098AX' , '10011T100000000098AY' , '10011T100000000098AZ' )
+      or T1.pk_postseries in ( '10011T100000000098AR' , '10011T100000000098B0' , '10011T100000000098BB' , '10011T100000000098BP' , '10011T100000000098BQ' , '10011T100000000098BT' , '10011T100000000098BU' , '10011T100000000098BV' , '10011T100000000098BW' , '10011T100000000098BX' , '10011T100000000098BY' , '10011T100000000098BZ' , '10011T100000000098C0' , '10011T100000000098C1' , '10011T100000000098C2' , '10011T100000000098C3' , '10011T1000000000A73M' , '10011T100000000098B1' , '10011T100000000098C4' , '10011T100000000098C5' , '10011T100000000098C6' , '10011T100000000098C7' , '10011T100000000098CH' , '10011T100000000098CI' , '10011T100000000098CJ' , '10011T100000000098CK' , '10011T100000000098CL' , '10011T100000000098CM' , '10011T100000000098CN' , '10011T100000000098CO' , '10011T100000000098CP' , '10011T100000000098CQ' , '10011T100000000098CR' , '10011T100000000098CS' , '10011T100000000098CT' , '10011T100000000098CU' , '10011T100000000098DP' , '10011T100000000098DQ' , '10011T100000000098DR' , '10011T100000000098DS' , '10011T100000000098DT' , '10011T100000000098DU' , '10011T100000000098B7' , '10011T100000000098DV' , '10011T100000000098DW' , '10011T100000000098DX' , '10011T100000000098DY' , '10011T100000000098DZ' , '10011T100000000098E0' , '10011T100000000098E1' , '10011T100000000098E2' , '10011T100000000098E3' , '10011T100000000098BA' )
+  )
   -- 去重：对每个人在每个组织类型+岗位序列下，在时间段内选择最新的记录
   and T1.pk_psnjob = (
     select top 1 T1_dedup.pk_psnjob
@@ -101,55 +106,24 @@ from (
     left outer join bd_defdoc bd_defdoc_dwszd_dedup on T2_dedup.def4 = bd_defdoc_dwszd_dedup.pk_defdoc
     where T1_dedup.pk_psndoc = T1.pk_psndoc
       and T1_dedup.ismainjob='Y'
-      -- 时间段内的记录
-      and T1_dedup.begindate<=parameter('param2') and nvl(T1_dedup.enddate, '2099-12-31') >= parameter('param1')
+    -- 单日内有效的记录
+    and T1_dedup.begindate<=parameter('rq') and nvl(T1_dedup.enddate, '2099-12-31') >= parameter('rq')
       -- 必须满足主查询的筛选条件（关键修改）
       and bd_psncl_dedup.name in (parameter('rylb'))
       and bd_defdoc_zzlx_dedup.name in (parameter('zzlx'))
+      and bd_defdoc_dwszd_dedup.name in (parameter('dwszd'))
+      and bd_defdoc_zzlx_dedup.name is not null
+      and bd_defdoc_dwszd_dedup.name is not null
       and T2_dedup.name in (parameter('zzmc'))
-      -- 同一组织类型（母公司/京内子公司/京外子公司/境外子公司）
-      and (
-        case when bd_defdoc_zzlx_dedup.name in ('总部','分公司','专业机构','事业部') then '母公司'
-             when bd_defdoc_zzlx_dedup.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd_dedup.name ='京内' then '京内子公司'
-             when bd_defdoc_zzlx_dedup.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd_dedup.name ='京外' then '京外子公司'
-             when bd_defdoc_zzlx_dedup.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd_dedup.name ='境外' then '境外子公司'
-        end
-      ) = (
-        case when bd_defdoc_zzlx.name in ('总部','分公司','专业机构','事业部') then '母公司'
-             when bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京内' then '京内子公司'
-             when bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京外' then '京外子公司'
-             when bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='境外' then '境外子公司'
-        end
-      )
+      -- 同一组织名称
+      and T2_dedup.name = T2.name
       -- 同一岗位序列
       and T1_dedup.pk_postseries = T1.pk_postseries
     order by T1_dedup.enddate desc, T1_dedup.begindate desc, T1_dedup.ts desc
   )
   group by 
-    case when  bd_defdoc_zzlx.name in ('总部','分公司','专业机构','事业部') then '母公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京内' then '京内子公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京外' then '京外子公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='境外' then '境外子公司'
-    end,
+    T2.name,
     case when T1.pk_postseries in ( '10011T100000000098AQ' , '10011T100000000098AT' , '10011T100000000098AU' , '10011T100000000098AV' , '10011T100000000098AW' , '10011T100000000098AX' , '10011T100000000098AY' , '10011T100000000098AZ' ) then '经营管理'
          when T1.pk_postseries in ( '10011T100000000098AR' , '10011T100000000098B0' , '10011T100000000098BB' , '10011T100000000098BP' , '10011T100000000098BQ' , '10011T100000000098BT' , '10011T100000000098BU' , '10011T100000000098BV' , '10011T100000000098BW' , '10011T100000000098BX' , '10011T100000000098BY' , '10011T100000000098BZ' , '10011T100000000098C0' , '10011T100000000098C1' , '10011T100000000098C2' , '10011T100000000098C3' , '10011T1000000000A73M' , '10011T100000000098B1' , '10011T100000000098C4' , '10011T100000000098C5' , '10011T100000000098C6' , '10011T100000000098C7' , '10011T100000000098CH' , '10011T100000000098CI' , '10011T100000000098CJ' , '10011T100000000098CK' , '10011T100000000098CL' , '10011T100000000098CM' , '10011T100000000098CN' , '10011T100000000098CO' , '10011T100000000098CP' , '10011T100000000098CQ' , '10011T100000000098CR' , '10011T100000000098CS' , '10011T100000000098CT' , '10011T100000000098CU' , '10011T100000000098DP' , '10011T100000000098DQ' , '10011T100000000098DR' , '10011T100000000098DS' , '10011T100000000098DT' , '10011T100000000098DU' , '10011T100000000098B7' , '10011T100000000098DV' , '10011T100000000098DW' , '10011T100000000098DX' , '10011T100000000098DY' , '10011T100000000098DZ' , '10011T100000000098E0' , '10011T100000000098E1' , '10011T100000000098E2' , '10011T100000000098E3' , '10011T100000000098BA' ) then '专业技术' end
 ) agg
-join (
-  select distinct 
-    T2.name as orgname,
-    case when  bd_defdoc_zzlx.name in ('总部','分公司','专业机构','事业部') then '母公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京内' then '京内子公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='京外' then '京外子公司'
-         when  bd_defdoc_zzlx.name in ('子公司','子公司下属分公司','子公司下属子公司') and bd_defdoc_dwszd.name ='境外' then '境外子公司'
-    end as gslx
-  from 
-    hi_psnjob T1
-    left outer join org_adminorg T2 on T2.pk_adminorg = T1.pk_org 
-    left outer join bd_defdoc bd_defdoc_zzlx on T2.def2 = bd_defdoc_zzlx.pk_defdoc
-    left outer join bd_defdoc bd_defdoc_dwszd on T2.def4 = bd_defdoc_dwszd.pk_defdoc
-  where 1 = 1
-    and T1.lastflag='Y' and T1.ismainjob='Y'
-    and T1.begindate<=parameter('param2') and nvl(T1.enddate, '2099-12-31') >= parameter('param1')
-    and bd_defdoc_zzlx.name in (parameter('zzlx'))
-    and T2.name in (parameter('zzmc'))
-) orgs on orgs.gslx = agg.gslx
+where agg.gwxl in (parameter('gwxl'))
