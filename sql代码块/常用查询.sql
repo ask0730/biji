6ebb81e13c0bc45ceb844a81d4c898d4d4a9e426
ask <2137576215@qq.com>
2025-12-09 16:46:16 +0800

    常用查询

--- sql代码块/常用查询.sql
+++ sql代码块/常用查询.sql
@@ -4,6 +4,9 @@
 
 - select *from view_NSRJCXX
 
+
+
+
 - 使用解决方案附件脚本，清除该人员缓存数据，然后再到【考勤规则】节点，点击列表记录上的“手工排班”按钮，重新排班，重新生成日报数据。
 --查询
 select * from ts_staff_calendar_history where STAFF_ID =(select pk_psndoc from bd_psndoc where code = '00002306') and CALENDAR >= CONVERT(DATETIME, '2025-02-01 00:00:00', 120) and CALENDAR <= CONVERT(DATETIME, '2025-02-28 00:00:00', 120);--固定班制排班记录
