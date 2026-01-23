1e33f968829fea981bd4712c3767293cd7822ecd
ask <2137576215@qq.com>
2026-01-23 15:03:35 +0800

    部门信息

---
+++ sql代码块/部门信息.sql
@@ -0,0 +1,13 @@
+SELECT
+  d.code  AS dept_code,
+  d.name  AS dept_name,
+  p.code  AS parent_dept_code,
+  p.name  AS parent_dept_name,
+  o.code  AS org_code,
+  o.name  AS org_name
+FROM org_dept d
+LEFT JOIN org_dept p
+  ON p.pk_dept = d.pk_fatherorg
+LEFT JOIN org_orgs o
+  ON o.pk_org = d.pk_org
+ORDER BY o.code, ISNULL(p.code, '0'), d.code;
